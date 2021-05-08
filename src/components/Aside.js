416f9d8be9ebfcec3dd2c64e0a2d168667616f49
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-08 09:57:24 +0300

    aside created with jsx

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom"
 const Aside = () => {
   return (
     <div>
+        
       <h3 className="aside__title">STEPS</h3>
       <ul>
         <li className="aside__link">
