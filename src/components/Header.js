c39d0312d55311ef859b77184984049abef1b874
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 22:03:19 +0300

    Arrangement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,29 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="nav-wrap">
+        <nav className="nav">
+          <div className="event">
+            <NavLink exact to="/event" className="nav__item">
+              Event Details
+            </NavLink>
+            <span className="material-icons">expand_more</span>
+          </div>
+          <NavLink exact to="/registrationForm" className="nav__item">
+            Registration Form
+          </NavLink>
+        </nav>
 
-const Header =() => {
-    return (
-            <div className="container" >
-                <div className="nav-wrap">
-                    <nav className="nav">
-                        <div className="event">
-                            <NavLink exact to="/Event" className="nav__item">Event Details</NavLink>
-                            <span className="material-icons">
-                                expand_more
-                            </span>
-                        </div>
-                        <NavLink exact to="/RegistrationForm" className="nav__item">Registration Form</NavLink>
-                    </nav>
-                   
-                    <div >
-                            <a href="#" className="button" > Sign In</a>
-                    </div>
-                </div>
-            </div>
-    )
+        <div>
+          <a href="/#" className="button">
+            Sign In
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
