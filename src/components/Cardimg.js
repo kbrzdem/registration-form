6efb1ef51b46465e05171ca72ff2a236bcabb179
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 22:02:34 +0300

    img comp created

---
+++ src/components/Cardimg.js
@@ -0,0 +1,16 @@
+const Cardimg = () => {
+  return (
+    <div className="cardimg-wrap">
+      <figure className="card-img m-0">
+        <img
+          style={{ width: "100%" }}
+          src="image/Event-banner.png"
+          className=""
+          alt=""
+        />
+      </figure>
+    </div>
+  );
+};
+
+export default Cardimg;
