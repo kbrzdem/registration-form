5e58dfd07f7b5d8bcb98288da4f208d53273b895
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 05:30:05 +0300

    registration comp created

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,25 @@
+import Aside from "./Aside";
+import RegistrationType from "./RegistrationType"
+
+
 const RegistrationForm =() => {
     return (
-            <div><h1>registtnmds</h1></div>
+      
+                
+       
+          <div className="row">
+              <aside className="aside col-12 col-md-4 col-lg-3">
+                <Aside />
+              </aside>
+              <div className="col-12 col-md-4 col-lg-6">
+            <section className="type">
+              <RegistrationType/>
+            </section>
+            
+            </div>
+          </div>
+      
+      
     )
 };
 
