dc001117eb841b5eddc32807ad7a14d7823bd5ce
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 08:08:49 +0300

    issuing cards

--- src/components/RegistrationType.js
+++ src/components/RegistrationType.js
@@ -15,23 +15,28 @@ const RegistrationType = () => {
 
   return (
     <>
-      <h1 className="choosetitle"> Choose Registration Types </h1>
+      <h1 className="choosetitle"> Choose Registration Types {radio} </h1>
       <form onSubmit={handleSubmitForm} className="global-form">
-        <div className="row">
+        <div className="row typecard">
           <div className="col-12">
             <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="regular"
-                  className="formradio"
-                  checked={radio === "regular"}
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Regular
-                <p>
+              <label className="radio-label">
+                <div className="typeradio-title-wrap">
+                  <div className="typeradio-title">
+                    <input
+                      type="radio"
+                      value="regular"
+                      className="typeform-radio"
+                      checked={radio === "regular"}
+                      onChange={(e) => {
+                        setRadio(e.target.value);
+                      }}
+                    />
+                    <h3 className="type-radio-title">Regular</h3>
+                  </div>
+                  <p className="type-radio-number">$65,00</p>
+                </div>
+                <p className="typeradio-text">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -45,21 +50,26 @@ const RegistrationType = () => {
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row typecard">
           <div className="col-12">
             <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="student"
-                  className="formradio"
-                  checked={radio === "student"}
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Student
-                <p>
+              <label className="radio-label">
+                <div className="typeradio-title-wrap">
+                  <div className="typeradio-title">
+                    <input
+                      type="radio"
+                      value="student"
+                      className="formradio"
+                      checked={radio === "student"}
+                      onChange={(e) => {
+                        setRadio(e.target.value);
+                      }}
+                    />
+                    <h3 className="type-radio-title">Student</h3>
+                  </div>
+                  <p className="type-radio-number">$65,00</p>
+                </div>
+                <p className="typeradio-text">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -69,21 +79,26 @@ const RegistrationType = () => {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12">
+        <div className="row typecard">
+          <div className="col-12 ">
             <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="industry"
-                  className="formradio"
-                  checked={radio === "industry"}
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Industry
-                <p>
+              <label className="radio-label">
+                <div className="typeradio-title-wrap">
+                  <div className="typeradio-title">
+                    <input
+                      type="radio"
+                      value="industry"
+                      className="formradio"
+                      checked={radio === "industry"}
+                      onChange={(e) => {
+                        setRadio(e.target.value);
+                      }}
+                    />
+                    <h3 className="type-radio-title">Industry</h3>
+                  </div>
+                  <p className="type-radio-number">$65,00</p>
+                </div>
+                <p className="typeradio-text">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -97,20 +112,25 @@ const RegistrationType = () => {
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row typecard">
           <div className="col-12">
             <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="I’ve a Verification Code"
-                  className="formradio"
-                  checked={radio === "I’ve a Verification Code"}
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                I’ve a Verification Code
+              <label className="radio-label">
+                <div className="typeradio-title-wrap">
+                  <div className="typeradio-title">
+                    <input
+                      type="radio"
+                      value="I’ve a Verification Code"
+                      className="formradio"
+                      checked={radio === "I’ve a Verification Code"}
+                      onChange={(e) => {
+                        setRadio(e.target.value);
+                      }}
+                    />
+                    <h3>I’ve a Verification Code</h3>
+                  </div>
+                  <p className="type-radio-number">$65,00</p>
+                </div>
               </label>
             </div>
           </div>
