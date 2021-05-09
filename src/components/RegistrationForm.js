c4a0249a3e5ff5cc723a2b88c8b98bd71defdcca
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 22:03:51 +0300

    container-fluid used

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,26 +1,25 @@
 import Aside from "./Aside";
-import RegistrationType from "./RegistrationType"
+import RegistrationType from "./RegistrationType";
+import Cardimg from "./Cardimg";
 
-
-const RegistrationForm =() => {
-    return (
-      
-                
-       
-          <div className="row">
-              <aside className="aside col-12 col-md-4 col-lg-3">
-                <Aside />
-              </aside>
-              <div className="col-12 col-md-4 col-lg-6">
-            <section className="type">
-              <RegistrationType/>
-            </section>
-            
-            </div>
-          </div>
-      
-      
-    )
+const RegistrationForm = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <aside className="aside col-12 col-md-4 col-lg-3">
+          <Aside />
+        </aside>
+        <div className="col-12 col-md-4 col-lg-6">
+          <section className="type">
+            <RegistrationType />
+          </section>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <Cardimg />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default RegistrationForm
+export default RegistrationForm;
