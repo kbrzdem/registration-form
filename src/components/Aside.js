9e696183c9e31d2eca13a2b91de0d4efd42ea26e
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-09 05:30:37 +0300

    aside comp created

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,19 +1,46 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+
+//import RegistrationType from "./RegistrationType"
 
 const Aside = () => {
   return (
-    <div>
-        
+    <div className="">
       <h3 className="aside__title">STEPS</h3>
       <ul>
-        <li className="aside__link">
-          <NavLink exact to="/">Registration Type</NavLink>{" "}
+        <li>
+          <div className="aside__link">
+            <NavLink
+              NavLink
+              exact
+              to="/RegistrationForm"
+              className="aside__link--text "
+            >
+              Registration Type
+            </NavLink>
+          </div>
         </li>
-        <li className="aside__link">
-          <NavLink exact to="/">Questions</NavLink>{" "}
+
+        <li>
+          <div className="aside__link">
+            <NavLink
+              exact
+              to="/RegistrationForm/qestion"
+              className="aside__link--text "
+            >
+              Questions
+            </NavLink>
+          </div>
         </li>
-        <li className="aside__link">
-          <NavLink exact to="/">Summary</NavLink>{" "}
+        <li>
+          <div className="aside__link">
+            <NavLink
+              exact
+              to="/RegistrationForm/summary"
+              className="aside__link--text "
+            >
+              Summary
+            </NavLink>
+          </div>
         </li>
       </ul>
     </div>
