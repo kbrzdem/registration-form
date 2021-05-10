83f28701ce7c584c8421d831c3810302d7258f42
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-10 06:34:29 +0300

    summary comp created

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,11 +1,52 @@
 import { useLocation } from "react-router-dom";
+import Aside from "./Aside";
 
-const Summary = (props) => {
+import Cardimg from "./Cardimg";
+
+const Summary = () => {
   const location = useLocation();
   return (
     <div>
-      <h1>{location.state.name}</h1>
-      <h2>{location.state.lastname}</h2>
+      <div className="container-fluid">
+        <div className="row">
+          <aside className="aside col-12 col-md-4 col-lg-3">
+            <Aside />
+          </aside>
+          <div className="col-12 col-md-4 col-lg-6">
+            <section>
+              <div className="row">
+                <h3 className="col-12 summary-title">Summary</h3>
+                <h4 className="col-12 summary-total">TOTAL AMOUNT:</h4>
+              </div>
+              <div className="row">
+                <div className="col-12 typecard typecard--summary-pd">
+                  <div className="summary-card">
+                    <p className="summary-text">
+                      {location.state.name} {location.state.lastname}
+                    </p>
+                    <h3 className="summary-subtitle">Registration Type </h3>
+                    <p className="summary-typetext">Regular</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <Cardimg />
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        className="next-wrap"
+      >
+        <div>
+          <button type="submit" className="btn">
+            Finish <span class="material-icons">east</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
